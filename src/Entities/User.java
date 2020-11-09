e69ef8219c04f95e1d90492e4a75276e3b1e6aae
Felipe <felipebruckmann@hotmail.com.br>
2020-11-09 18:33:48 -0300

    adicionando entities e classe user

---
+++ src/Entities/User.java
@@ -0,0 +1,17 @@
+package Entities;
+
+public abstract class User {
+
+    private int player_id;
+    private String name;
+    private String password;
+    private int age;
+    private String serie;
+    private boolean tdah;
+
+    protected User () {
+        
+    }
+
+
+}
