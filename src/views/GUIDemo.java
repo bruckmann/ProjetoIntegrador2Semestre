1a714b05bfabc347b92ffaf5593d43bc34d3a6c6
Joaquimlagos <64992814+Joaquimlagos@users.noreply.github.com>
2020-11-09 22:32:56 -0300

    criação views

--- src/views/GUIDemo.java
+++ src/views/GUIDemo.java
@@ -1,2 +1,12 @@
-package views;public class GUIDemo {
+package views;
+import javax.swing.JFrame;
+
+public class GUIDemo {
+    public static void main(String[] args) {
+        JFrame frame = null;
+
+        frame = new WindowPatternFrame();
+
+        frame.setVisible(true);
+    }
 }
--- src/views/WindowPatternFrame.java
+++ src/views/WindowPatternFrame.java
@@ -1,2 +1,15 @@
-package views;public class WindowPatternFrame {
+package views;
+import javax.swing.JFrame;
+
+public class WindowPatternFrame extends JFrame {
+
+    public WindowPatternFrame(){
+        this("Demo WindowPatternFrame");
+    }
+
+    public WindowPatternFrame(String title) {
+        super(title);
+        this.setSize(640,480);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 }
