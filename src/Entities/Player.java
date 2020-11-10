df8aa2fcfcde80083df4be44863f612d8d29ba9a
Felipe <felipebruckmann@hotmail.com.br>
2020-11-09 21:53:14 -0300

    Adicionando métodos getters as classes

--- src/Entities/Player.java
+++ src/Entities/Player.java
@@ -5,8 +5,8 @@ public class Player extends User {
     private String grade;
     private boolean tdah;
 
-    public Player(int player_id, String name, String password, int age, String email, String grade, boolean tdah) {
-        super(player_id, name, password, age, email);
+    public Player(int player_id, String name, String role, String password, int age, String email, String grade, boolean tdah) {
+        super(player_id, name, role, password, age, email);
         this.grade = grade;
         this.tdah = tdah;
     }
