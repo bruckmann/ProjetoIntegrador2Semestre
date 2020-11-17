3d30bb45c1df63a584f1e7078941fde1f5235074
Felipe <felipebruckmann@hotmail.com.br>
2020-11-17 18:46:42 -0300

    alternando nomes dos atributos do createQuestion

--- src/Entities/Admin.java
+++ src/Entities/Admin.java
@@ -2,8 +2,11 @@ package Entities;
 
 public class Admin extends User {
 
+    private String email;
+
     public Admin(int admin_id, String name, String role, String password, int age, String email){
-        super(admin_id, name, role, password, age, email);
+        super(admin_id, name, role, password, age);
+        this.email = email;
     }
 
 
--- src/views/RegisterAdm.java
+++ src/views/RegisterAdm.java
@@ -19,23 +19,23 @@ public class RegisterAdm extends StandartFormatLog {
 
         label = new JLabel("NOME: ");
         addComponet(label,2,0,1,1);
-        JTextField textField = new JTextField(20);
-        addComponet(textField,2,1,3,1);
+        JTextField name = new JTextField(20);
+        addComponet(name,2,1,3,1);
 
         label = new JLabel("SENHA: ");
         addComponet(label, 3,0,1,1);
-        JPasswordField passwordField = new JPasswordField(10);
-        addComponet(passwordField, 3,1,3,1);
+        JPasswordField password = new JPasswordField(10);
+        addComponet(password, 3,1,3,1);
 
         label = new JLabel("IDADE: ");
         addComponet(label, 4,0,1,1);
-        JTextField textField2 = new NumberMask(3);
-        addComponet(textField2,4,1,3,1);
+        JTextField yearsOld = new NumberMask(3);
+        addComponet(yearsOld,4,1,3,1);
 
         label = new JLabel("EMAIL: ");
         addComponet(label, 5,0,1,1);
-        JTextField textField3 = new JTextField(20);
-        addComponet(textField3,5,1,3,1);
+        JTextField email = new JTextField(20);
+        addComponet(email,5,1,3,1);
 
         JButton button = new JButton("CRIAR CONTA");
         button.setForeground(Color.BLUE);
