b39c2ba5c2aa16a81fd87658ef18620c886aaf92
Lipzs <61035595+Lipzs@users.noreply.github.com>
2020-11-17 22:23:11 -0300

    Validação de campos

--- src/views/RegisterAdm.java
+++ src/views/RegisterAdm.java
@@ -1,12 +1,17 @@
 package views;
 
+import Util.ViewHelper;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 
 public class RegisterAdm extends StandartFormatLog {
     private WindowManager frame;
+    private ViewHelper helper = new ViewHelper();
 
     public RegisterAdm(WindowManager windowManager){
 
@@ -23,28 +28,51 @@ public class RegisterAdm extends StandartFormatLog {
         label = new JLabel("NOME: ");
         addComponet(label,2,0,1,1);
         JTextField name = new JTextField(20);
+        name.setName("Nome");
         addComponet(name,2,1,3,1);
 
         label = new JLabel("SENHA: ");
         addComponet(label, 3,0,1,1);
         JPasswordField password = new JPasswordField(10);
+        password.setName("Senha");
         addComponet(password, 3,1,3,1);
 
         label = new JLabel("IDADE: ");
         addComponet(label, 4,0,1,1);
         JTextField yearsOld = new NumberMask(3);
+        yearsOld.setName("Idade");
         addComponet(yearsOld,4,1,3,1);
 
         label = new JLabel("EMAIL: ");
         addComponet(label, 5,0,1,1);
         JTextField email = new JTextField(20);
+        email.setName("Email");
         addComponet(email,5,1,3,1);
 
         JButton button = new JButton("CRIAR CONTA");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                frame.ReturnToLogPage();
+                java.util.List<JTextField> emptyFields;
+                List<JTextField> aJTextList = new ArrayList<JTextField>();
+                aJTextList.add(name);
+                aJTextList.add(yearsOld);
+                aJTextList.add(email);
+
+                emptyFields = helper.validateFields(aJTextList);
+                boolean isPssValid = password.getPassword().length > 0;
+
+                if(emptyFields.isEmpty() && isPssValid) {
+                    frame.ReturnToLogPage();
+                } else {
+                    for (JTextField field : emptyFields) {
+                        System.out.println("O campo " + field.getName() + " não está preenchido\n");
+                    }
+                    if (!isPssValid) {
+                        System.out.println("O campo " + password.getName() + " não está preenchido\n");
+                    }
+                    System.out.println("-----------------------");
+                }
             }
         });
         button.setForeground(Color.BLUE);
--- src/views/RegisterPlayer.java
+++ src/views/RegisterPlayer.java
@@ -1,14 +1,20 @@
 package views;
 
+import Entities.User;
+import Util.ViewHelper;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class RegisterPlayer extends StandartFormatLog {
     private WindowManager frame;
-
+    private ViewHelper helper = new ViewHelper();
 
     public RegisterPlayer(WindowManager windowManager){
 
@@ -26,22 +32,26 @@ public class RegisterPlayer extends StandartFormatLog {
 
         label = new JLabel("NOME: ");
         addComponet(label,2,0,1,1);
-        JTextField name = new JTextField(20););
-        JPasswordField password = new JPasswordField();
+        JTextField name = new JTextField(20);
+        name.setName("nome");
         addComponet(name,2,1,3,1);
 
         label = new JLabel("SENHA: ");
-        addComponet(label, 3,0,1,1;
+        addComponet(label, 3,0,1,1);
+        JPasswordField password = new JPasswordField();
+        password.setName("Senha");
         addComponet(password, 3,1,3,1);
 
         label = new JLabel("IDADE: ");
         addComponet(label, 4,0,1,1);
         JTextField yearsOld = new NumberMask(3);
+        yearsOld.setName("Idade");
         addComponet(yearsOld,4,1,3,1);
 
         label = new JLabel("SERIE: ");
         addComponet(label, 6,0,1,1);
         JTextField grade = new NumberMask(1);
+        grade.setName("Série");
         addComponet(grade,6,1,3,1);
 
         label = new JLabel("TDAH: ");
@@ -49,6 +59,7 @@ public class RegisterPlayer extends StandartFormatLog {
         panel = new JPanel();
         ButtonGroup buttonGroup = new ButtonGroup();
         JRadioButton jRadioButton = new JRadioButton("SIM");
+        jRadioButton.setName("Tdah");
         buttonGroup.add(jRadioButton);
         JRadioButton jRadioButton2 = new JRadioButton("NÃO");
         buttonGroup.add(jRadioButton2);
@@ -56,23 +67,40 @@ public class RegisterPlayer extends StandartFormatLog {
         panel.add(jRadioButton2);
         addComponet(panel,7,1,1,1);
 
-
-
         JButton button = new JButton("CRIAR CONTA");
         button.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String n = name.getText();
-                if (!n.isBlank()) {
+                List<JTextField> emptyFields;
+                boolean isButtonSelected;
+                List<JTextField> aJTextList = new ArrayList<JTextField>();
+                aJTextList.add(name);
+                aJTextList.add(yearsOld);
+                aJTextList.add(grade);
+
+                emptyFields = helper.validateFields(aJTextList);
+                boolean isPssValid = password.getPassword().length > 0;
+                isButtonSelected = (jRadioButton.isSelected()) || (jRadioButton2.isSelected());
+
+                if(emptyFields.isEmpty() && isButtonSelected && isPssValid) {
                     frame.ReturnToLogPage();
+                } else {
+                    for(JTextField field : emptyFields) {
+                        System.out.println("O campo " + field.getName() + " não está preenchido\n");
+                    }
+                    if(!isButtonSelected) {
+                        System.out.println("O botão " + jRadioButton.getName() + " não está selecionado\n");
+                    }
+                    if(!isPssValid) {
+                        System.out.println("O campo " + password.getName() + " não está preenchido\n");
+
+                    }
+                    System.out.println("-----------------------");
                 }
-                
             }
         });
         button.setForeground(Color.BLUE);
         addComponet(button, 10,1,1,1);
     }
-
-
 }
 
