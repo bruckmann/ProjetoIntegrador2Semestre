84d371b8b81137a4cfb22ea13832bc24898510c5
Lipzs <61035595+Lipzs@users.noreply.github.com>
2020-11-23 21:37:18 -0300

    Update Question.java

--- src/Entities/questions/Question.java
+++ src/Entities/questions/Question.java
@@ -5,13 +5,13 @@ import java.util.List;
 public class Question {
 
   private final int questionId;
-  private int type;
+  private String type;
   private String questionStatement;
   private List<Alternative> alternativesList;
 
   public Question(int questionId, String questionStatement, String type, List<Alternative> alternatives) {
     this.questionStatement = questionStatement;
-    this.setType(type);
+    this.type = type;
     this.alternativesList = alternatives;
     this.questionId = questionId;
   }
@@ -24,16 +24,8 @@ public class Question {
     return this.questionStatement;
   }
 
-  public void setType(String type) {
-    if(type.toLowerCase().equals("soma")) {
-      this.type = 1;
-    }
-    else if (type.toLowerCase().equals("subtracao")) {
-      this.type = 2;
-    }
-    else if (type.toLowerCase().equals("ambos")) {
-      this.type = 3;
-    }
+  public String getType() {
+      return this.type;
   }
 
   public int getRightAlternative() {
